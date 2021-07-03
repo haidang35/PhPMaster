dfab8a46867dd9585a0d2aaecaad5cf88ab456f9
Dang Jinner <nguyenhaidangyq@gmail.com>
2021-07-03 09:27:06 +0700

    update

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('postgres://gfnxmrtbyjlegl:b2413237604e8d790927d11882b2595f8a04d6de88bee1d9d4fcb92f58d5eaf2@ec2-3-89-0-52.compute-1.amazonaws.com:5432/d4ce4po7ssmvcr'),
+            'host' => env('DB_HOST', 'ec2-3-89-0-52.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd4ce4po7ssmvcr'),
+            'username' => env('DB_USERNAME', 'gfnxmrtbyjlegl'),
+            'password' => env('DB_PASSWORD', 'b2413237604e8d790927d11882b2595f8a04d6de88bee1d9d4fcb92f58d5eaf2'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
